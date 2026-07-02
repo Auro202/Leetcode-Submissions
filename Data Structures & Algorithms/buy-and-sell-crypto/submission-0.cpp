29263c0e5a736ec2501b22e82c63994bd13986c7
Auro202 <kashyapreddy2005@gmail.com>
2026-07-02 17:02:31 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int l = 0, r = 1;
+        int ans = 0;
+        while(r < prices.size()){
+            if(prices[l] < prices[r]){
+                ans = max(prices[r] - prices[l], ans);
+                r++;
+            }else{
+                l = r;
+                r++;
+            }
+        }
+        return ans;
+    }
+};
